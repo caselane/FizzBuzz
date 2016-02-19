9809b327e8549fe7bbcbaf0d4d661ae00b3436c0
Case Rosenfelt <rosenfelt@wisc.edu>
2016-02-18 20:27:37 -0700

    FizzBuzz Refactor and prompts

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-for (var FizzBuzz = 1; FizzBuzz < 101; FizzBuzz++) {
-	if (FizzBuzz % 15 == 0) {
-		console.log("fizz buzz");
+$(document).ready(function() {
+	var countUp =  prompt('Write your number here:');
+	var x = parseInt(countUp);
+	for ( FizzBuzz = 1; FizzBuzz <= x; FizzBuzz++) {
+		if (FizzBuzz % 15 == 0) {
+			console.log("fizz buzz");
+			$('<p>fizz buzz</p>').appendTo('.numbers');
+		}
+		else if (FizzBuzz % 3 == 0) {
+			console.log("fizz");
+			$('<p>fizz</p>').appendTo('.numbers');
+		}
+		else if (FizzBuzz % 5 == 0) {
+			console.log("buzz");
+			$('<p>buzz</p>').appendTo('.numbers');
+		}
+		else {
+			console.log(FizzBuzz);
+			$('<p>' + FizzBuzz + '</p>').appendTo('.numbers');
+		}
 	}
-	else if (FizzBuzz % 3 == 0) {
-		console.log("fizz");
-	}
-	else if (FizzBuzz % 5 == 0) {
-		console.log("buzz");
-	}
-	else {
-		console.log(FizzBuzz);
-	}
-}
+});
